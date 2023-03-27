463e0f93c2c54d49176df39cd5f7e2b303770080
JuliaBolting <boltingjulia@gmail.com>
2023-03-27 19:32:49 -0300

    Começado lista de exercicios 5

---
+++ ListaExerciciosAula5/src/listaexerciciosaula5/Exercicio7.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package listaexerciciosaula5;
+
+/**
+Crie um programa para auxiliar engenheiros a calcular a quantidade de piso a ser
+utilizado em uma sala retangular, a quantidade de tinta a ser gasta nas paredes, bem
+como a potência do ar-condicionado. Para isto, o programa deve ler o comprimento, a
+largura e a altura da sala, calcular e imprimir a área de piso da sala, a área total das
+paredes e o volume da sala. O programa deve mostrar também o tamanho do aparelho de
+ar-condicionado a ser instalado. Um pequeno deve ser instalado se o volume da sala for
+inferior a 100 m3; 
+um aparelho médio deve ser instalado se o volume da sala estiver entre 100 e 500 m3; 
+e um aparelho grande deve ser instalado caso o volume seja superior a 500 m3
+ */
+public class Exercicio7 {
+    
+}
