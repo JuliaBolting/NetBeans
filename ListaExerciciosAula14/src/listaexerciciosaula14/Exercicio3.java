71f54d6116233b754ada5246c79105f46a87e1ee
JuliaBolting <boltingjulia@gmail.com>
2023-05-16 21:44:09 -0300

    Lista de exercicios aula 14

--- ListaExerciciosAula14/src/listaexerciciosaula14/Exercicio3.java
+++ ListaExerciciosAula14/src/listaexerciciosaula14/Exercicio3.java
@@ -1,18 +1,18 @@
 package listaexerciciosaula14;
 
 /**
- * Faça um programa que mostre na tela todos os números inteiros de 1 a 100 .
+Faça um programa que mostre na tela os números ímpares de 1 a 100.
  */
-public class Exercicio1 {
+public class Exercicio3 {
 
     public static void main(String[] args) {
         int num = 1;
 
         do {
             System.out.println(num);
-            num++;
+            num = num+2;
         } while (num <= 100);
 
     }
-
+    
 }
