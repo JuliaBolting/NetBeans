df6244315578704a8527e5c7ca25905a31ffcdc3
JuliaBolting <boltingjulia@gmail.com>
2023-05-16 21:43:05 -0300

    alterações Exercicios aula 14

--- ExercicioAula14/src/exercicioaula14/Exercicio1.java
+++ ExercicioAula14/src/exercicioaula14/Exercicio1.java
@@ -1,12 +1,13 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package exercicioaula14;
 
+import java.util.Scanner;
+
 /**
- *
- * @author Julia
+Faça um programa que leia o nome e a
+idade de uma pessoa e apresente quantos anos esta
+pessoa terá daqui cinco anos. A idade digitada deve
+ser um valor entre 0 e 100, caso contrário o programa
+deve solicitar que o usuário digite a idade novamente.
  */
 public class Exercicio1 {
 
@@ -14,7 +15,21 @@ public class Exercicio1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner input = new Scanner(System.in);
+        String nome;
+        int idade;
+
+        System.out.println("Insira seu nome: ");
+        nome = input.nextLine();
+
+        do{
+        System.out.println("Insira sua idade: ");
+        idade = Integer.parseInt(input.nextLine());
+        }while(idade < 0 || idade > 100);
+        
+        System.out.println("A idade de " + nome + " daqui a 5 anos: " + (idade + 5));
+        
+        
     }
     
 }
